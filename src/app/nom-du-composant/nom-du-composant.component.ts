90a82965eb3f270816071fece52f9307101295fe
Foubert Antoine <ant.foub@gmail.com>
2025-05-07 11:40:39 +0200

    exo2 fini

---
+++ src/app/nom-du-composant/nom-du-composant.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nom-du-composant',
+  imports: [],
+  templateUrl: './nom-du-composant.component.html',
+  styleUrl: './nom-du-composant.component.scss'
+})
+export class NomDuComposantComponent {
+
+}
