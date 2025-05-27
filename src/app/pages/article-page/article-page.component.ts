64a32c1a7019864f80ed824ccca7f572f602a561
Foubert Antoine <ant.foub@gmail.com>
2025-05-27 16:19:27 +0200

    Update article-page.component.ts

--- src/app/pages/article-page/article-page.component.ts
+++ src/app/pages/article-page/article-page.component.ts
@@ -1,19 +1,21 @@
-import {Component, NgIterable, OnInit} from '@angular/core';
-import {ActivatedRoute} from '@angular/router';
-import {AsyncPipe, DatePipe, NgForOf, NgIf} from '@angular/common';
+import {Component, computed, inject, input, numberAttribute} from '@angular/core';
+import {ArticleStore} from '../../article.store';
+import {RouterLink} from '@angular/router';
+import {DatePipe} from '@angular/common';
 
 @Component({
   selector: 'app-article-page',
-  imports: [],
+  imports: [
+    RouterLink,
+    DatePipe
+  ],
   templateUrl: './article-page.component.html',
   styleUrl: './article-page.component.scss'
 })
-export class ArticlePageComponent implements OnInit {
-  articleId: string | null = null;
-
-  constructor(private route: ActivatedRoute) {}
-
-  ngOnInit(): void {
-    this.articleId = this.route.snapshot.paramMap.get('id');
-  }
+export class ArticlePageComponent {
+  readonly #articleStore = inject(ArticleStore);
+  readonly id = input.required({transform: numberAttribute});
+  readonly article = computed(() => this.#articleStore.articles().find(article => article.id === this.id()));
 }
+
+
